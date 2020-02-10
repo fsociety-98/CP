ece1e64b62a5477836563481fdd5d094fce7a3da
Sumanth <vsk21098@gmail.com>
2020-02-10 10:40:59 +0530

    Changed HackerEarth

--- HackerEarth/StudentArrangement.cpp
+++ HackerEarth/StudentArrangement.cpp
@@ -1,87 +1,59 @@
+
 #include<bits/stdc++.h>
+
 using namespace std;
 
 #define ll long long
 
-
-ll findNextFreeRow(vector<ll>& dp,vector<ll> &start,ll pref,ll k)
-{
-
-	ll count=0,i=start[pref];
-
-	while(true)
-	{
-		if(dp[i]<k)
-			{
-				start[pref]=i;
-				return i;
-			}
-
-		i = (i+1 == dp.size())?0:i+1;
-		count++;
-
-		if(count>dp.size())
-			break;
-	}
-	return -1;
-}
-
-
-
-ll countMisplaced(ll n,ll m,ll k,vector<ll>& pref)
-{
-
-	//DP to store number of seats occupied in each row
-	vector<ll> dp(m,0);
-	vector<ll> start(m,0);
-
-	ll i,place;
-	ll count = 0;
-
-	//Initialize
-	for(i=0;i<m;i++)
-		start[i] = i;
-
-
-	for(i=0;i<n;i++)
-	{
-
-		if(dp[pref[i]-1] < k)
-		{
-			dp[pref[i]-1] = dp[pref[i]-1] + 1;
-		}
-		else
-		{
-			count++;
-
-			//Place the student
-			place = findNextFreeRow(dp,start,pref[i]-1,k);
-
-
-			if(place !=-1)
-				dp[place] = dp[place]+1;
-			else
-			{
-			    count = count + (n - i - 1);
-			    break;
-			}
-		}
-
-	}
-	return count;
-}
 int main()
 {
-	ll n,m,k;
-
+	ll n,m,k,ch,count=0;
+	
 	cin>>n>>m>>k;
-
-	vector<ll> pref(n);
-
-	for(ll i=0;i<n;i++)
-		cin>>pref[i];
-
-	cout<<countMisplaced(n,m,k,pref)<<endl;
-
+	
+	unordered_map<ll,ll> dp;
+	unordered_map<ll,ll>::iterator itr,itr2;
+	for(int i=0;i<n;i++)
+	{
+		cin>>ch;
+		itr = dp.find(ch);
+		if(itr == dp.end())
+			dp.insert(make_pair(ch,1));
+		else
+			{
+				if(itr->second == k)
+				{
+					count++;
+					
+					ll cur_ch = (ch==n)?1:ch+1;
+					itr2 = dp.find(cur_ch);
+					
+					if(itr2 == dp.end())
+						{
+							dp.insert(make_pair(cur_ch,1));
+							continue;
+						}
+					ll inner_count=0;
+					while(inner_count<n)
+					{
+						inner_count++;
+						if(itr2->second <k)
+							{
+								itr2->second = itr2->second+1;
+								break;
+							}
+						else
+							{
+								cur_ch = (cur_ch==n)?1:cur_ch+1;
+								itr2 = dp.find(cur_ch);
+								continue;
+							}
+					}
+				}
+				else
+					itr->second = itr->second+1;
+			}
+	}
+	cout<<count<<endl;
 	return 0;
 }
